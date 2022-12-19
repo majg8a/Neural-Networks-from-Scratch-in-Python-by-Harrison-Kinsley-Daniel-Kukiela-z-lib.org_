40480da3616ab988078dbc2f6b33ee77f31fd1cd
Marcelo Gil <majgil@grupoctg.local>
2022-12-19 17:30:46 -0400

    feat: page 55

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import numpy as np
 weights1 = [0.2, 0.8, -0.5, 1]
 weights2 = [0.5, -0.91, 0.26, -0.5]
 weights3 = [-0.26, -0.27, 0.17, 0.87]
@@ -33,3 +34,34 @@ weigths = [[0.2, 0.8, -0.5, 1]
 biases = [2, 3, .5]
 
 layer_outputs = []
+
+for neuron_weigths, neuron_bias in zip(weigths, biases):
+    neuron_output = 0
+    for n_input, weigth in zip(inputs, neuron_weigths):
+        neuron_output += n_input*weigth
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
+
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
+
+outputs = np.dot(weights, inputs) + bias
+print(outputs)
+
+
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2.0, 3.0, 0.5]
+
+layer_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+print(layer_outputs)
