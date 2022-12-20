a26ed7f64e4b75e806dfaf9c6f6354e9457b1779
Marcelo Gil <majgil@grupoctg.local>
2022-12-20 17:28:43 -0400

    feat: page 116

--- script.py
+++ script.py
@@ -78,3 +78,75 @@ biases2 = [-1, 2, -0.5]
 layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
 layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
 print(layer2_outputs)
+
+
+# Softmax activation
+class Activation_Softmax:
+    # Forward pass
+    def forward(self, inputs):
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                            keepdims=True))
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+                                            keepdims=True)
+        self.output = probabilities
+
+
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
+
+# Create Dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense(2, 3)
+
+# Create ReLU activation (to be used with Dense layer):
+activation1 = Activation_ReLU()
+
+# Create second Dense layer with 3 input features (as we take output
+# of previous layer here) and 3 output values (output values)
+dense2 = Layer_Dense(3, 3)
+
+# Create Softmax activation (to be used with Dense layer):
+activation2 = Activation_Softmax()
+
+# Make a forward pass of our training data through this layer
+dense1.forward(X)
+
+# Make a forward pass through activation function
+# it takes the output of first dense layer here
+activation1.forward(dense1.output)
+
+# Make a forward pass through second Dense layer
+# it takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+
+# Make a forward pass through activation function
+# it takes the output of second dense layer here
+activation2.forward(dense2.output)
+
+# Let's see output of the first few samples:
+print(activation2.output[:5])
+
+
+softmax_outputs = np.array([[0.7, 0.1, 0.2],
+                            [0.1, 0.5, 0.4],
+                            [0.02, 0.9, 0.08]])
+class_targets = np.array([[1, 0, 0],
+                          [0, 1, 0],
+                          [0, 1, 0]])
+# Probabilities for target values -
+# only if categorical labels
+if len(class_targets.shape) == 1:
+    correct_confidences = softmax_outputs[
+        range(len(softmax_outputs)),
+        class_targets
+    ]
+elif len(class_targets.shape) == 2:
+    correct_confidences = np.sum(
+        softmax_outputs * class_targets,
+        axis=1
+    )
+# Losses
+neg_log = -np.log(correct_confidences)
+average_loss = np.mean(neg_log)
+print(average_loss)
